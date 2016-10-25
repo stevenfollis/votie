ab1dc3bfcdccd33e3b8bc3bbe88b7053a3e1cd8f
Steven Follis <steven_follis@live.com>
2016-10-25 18:09:34 -0400

    Added privacy statement

--- app.js
+++ app.js
@@ -51,13 +51,19 @@ var dialogs = require('./dialogs');
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url);
+  console.log('%s listening to %s', server.name, server.url);
 });
+
+// Privacy Statement
+// TODO: Formalize
+server.get('/privacy', (req, res) => {
+  res.send('Votie does not collect or persist any personal information beyond the user submitted address.');
+})
 
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+  appId: process.env.MICROSOFT_APP_ID,
+  appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
